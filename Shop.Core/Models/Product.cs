3a611db733d6c7339970c18e85a98204a363cbe8
Quentin Bihour <quentin.bihour@yahoo.fr>
2021-01-19 09:53:17 +0100

    Models et couche acces en cache

---
+++ Shop.Core/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+        public string Image { get; set; }
+    }
+}
